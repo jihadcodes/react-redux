0d162f949b40661f3e447d4d7e9fa4d706225a22
jihadcodes <jihad.dev.pro@gmail.com>
2026-02-17 22:20:17 +0600

    button create

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,22 @@
 
-
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment, reset } from '../../features/counterSlice'
 
 const Header = () => {
     const count = useSelector((state) => state.counter.value)
-    console.log(count);
+    const dispatch = useDispatch()
     
   return (
-    <div>
+    <>
+    <div className='p-20'>
       <h1>Header Component</h1>
       <h3>Count : {count}</h3>
+      <button className='mr-4 py-2 px-3 rounded-3xl bg-amber-700 text-white' onClick={() => dispatch(increment())}>Increment</button>
+      <button className='mr-4 py-2 px-3 rounded-3xl bg-amber-700 text-white'  onClick={() => dispatch(decrement())}>Decrement</button>
+      <button className='mr-4 py-2 px-3 rounded-3xl bg-amber-700 text-white'  onClick={() => dispatch(reset())}>Reset</button>
+      
     </div>
+    </>
   )
 }
 
--- src/features/counterSlice.js
+++ src/features/counterSlice.js
@@ -8,13 +8,13 @@ const counterSlice = createSlice({
     },
     reducers :{
         increment : (state) => {
-            state.value += 1
+            state.value += 1;
         },
         decrement : (state) => {
-            state.value -= 1
+            state.value -= 1;
         },
         reset : (state) => {
-            state.value = 0
+            state.value = 0;
         }
     }
 })
